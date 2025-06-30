b79db4deb1fb74472b63dba40345fb38c4d472fc
Zain Butt <buttzain1575@gmail.com>
2025-06-30 15:28:18 +0500

    Initial commit

---
+++ db.py
@@ -0,0 +1,25 @@
+# import pyodbc 
+# import os
+
+# connection = pyodbc.connect('DRIVER={SQL Server};'+
+#                           'Server=DESKTOP-LCNFE94\SQLEXPRESS;'+ 
+#                           'Database=HMS;'+
+#                           'Trusted_Connection=True')
+
+# # connection.autocommit = True
+# print("Connected to database")
+
+#### *************************************
+# cursor = connection.cursor()
+
+# os.system("cls") # to clear output screen
+
+# connection.execute("insert into Mytab values ('AliHassan', 24)")
+# connection.execute("update Mytab set Age=8 where Name='Wali'")
+# cursor.execute("select * from Mytab")
+
+# for data in cursor:
+#     print(data[0], data[1])
+
+# a =  
+# print(type(a))
---
+++ main.py
@@ -0,0 +1,347 @@
+import os
+
+class Customer: #**************************
+    id =0 
+    name = "no name"
+    password = "0000"
+
+    def __init__(self, id=0, n="Unknown", p="0"):
+        self.id = id
+        self.name= n
+        self.password = p
+
+    def show(self):
+        print(self.id, self.name, self.password)
+
+class Manager:  #*********************************
+    id =0 
+    name = "no name"
+    password = "0000"
+
+
+    def __init__(self, id=0, n="Unknown", p="0"):
+        self.id = id
+        self.name= n
+        self.password = p
+
+    def show(self):
+        print(self.id, self.name, self.password)
+
+
+
+class Room: #***************************************
+    id = 0 
+    type=  ""
+    status = True
+    cust_id = None
+
+    def __init__(self, i, t, st, cid):
+        self.id = i
+        self.type = t
+        self.status = st
+        self.cust_id = cid
+
+    def show(self):
+        print(f"{self.id}  {self.type}  {self.status}  {self.cust_id}")
+
+
+class Booking:
+    booking_id= 0
+    cust_id = 0 
+    room_id= 0
+
+    def __init__(self,bid,  cid, rid):
+        self.booking_id = bid
+        self.cust_id = cid
+        self.room_id = rid
+    def show(self):
+        print(f"{self.booking_id} {self.cust_id} {self.room_id}")
+
+class Application:
+    app_id = 0
+    cust_id = 0
+    room_id = 0
+    app_status = None
+
+    def __init__(self, apid, csid, rmid):
+        self.app_id = apid
+        self.cust_id=  csid
+        self.room_id=  rmid
+
+    def showApplication(self):
+        print("---------------")
+        print(f"Application ID: {self.app_id}")
+        print(f"Customer Id: {self.cust_id}")
+        print(f"Room ID: {self.room_id}")
+        if self.app_status == None:
+            print(f"Application Status: Pending")
+        elif self.app_status == False:
+            print(f"Application Status: Rejected")
+        elif self.app_status == True:
+            print(f"Application Status: Approved")
+        print("---------------")
+
+
+class HMS:
+    Roomlist = []
+    Bookings=  []
+    Applications = []
+    def __init__(self):
+
+        self.Roomlist.append(Room(101, "A", True, None))
+        self.Roomlist.append(Room(102, "A", True, None))
+        self.Roomlist.append(Room(103, "A", True, None))
+        self.Roomlist.append(Room(104, "A", True, None))
+        self.Roomlist.append(Room(201, "B", True, None))
+        self.Roomlist.append(Room(202, "B", True, None))
+
+    def RoomsDetail(self):
+        os.system("cls")
+        print("ID Type Available Customer")
+        for r in self.Roomlist:
+            r.show()
+        return 
+    
+    def displayAllApplication(self):
+        for app in self.Applications:
+            app.showApplication()
+
+
+    def makeBooking(self, cid, rid):  # make only booking request/application
+        for r in self.Roomlist:
+            if r.id == rid and r.status == True:
+                # tempint = self.Bookings.__len__()
+                # self.Bookings.append(Booking(tempint, cid, rid))
+                tempint = self.Applications.__len__()
+                self.Applications.append(Application(tempint, cid, rid))
+                r.status = False
+                r.cust_id = cid 
+                return
+            elif r.id == rid and r.status == False:
+                print("Room not available..!!") 
+                input("Enter any key to continue..!!")
+                return
+        print("Room Does not exist.. !!")
+        input("Enter any key to continue..!!")
+        return
+    
+    def approveBooking(self, cid , rid): # when application approved by manager. booking placed 
+        tempint = self.Bookings.__len__()
+        self.Bookings.append(Booking(tempint, cid, rid)) 
+         
+    
+    def showApplicationStatus(self, cid):
+        for b in self.Applications:
+            if b.cust_id == cid:
+                b.showApplication()
+        return
+    
+    def manageApplications(self):
+        tempint = int(input("Enter Application No: "))
+        for a in self.Applications:
+            if a.app_id == tempint:
+                temp = ""
+                temp = input("Enter Command : (Approved/A , Reject/R) ")
+                if temp == "A" or temp == "a":
+                    a.app_status = True
+                    print("Action Preformed..!!")
+                    return a.cust_id, a.room_id
+                elif temp == "r" or  temp == "R": 
+                    a.app_status = False
+                    print("Action Preformed..!!")
+                    return None, a.room_id
+                else:
+                    print("Invalid Input..!!")  
+                    return None, None
+
+        print("Application Does not exist..!!")
+    
+    def showbookings(self):
+        print("BookingId  CustomerId  Room-No")
+        for b in self.Bookings:
+            b.show()
+
+    def clearRoom(self, rid):
+        for r in  self.Roomlist:
+            if rid == r.id:
+                r.status = True
+                r.cust_id = None
+
+
+
+def signIn(tlist):  # accessing old account 
+    temp = input("Enter Username: ")
+    for m in tlist:
+        tempint= 0
+        if m.name == temp:
+            tempint = 1
+            temp1 = input("Enter Password: ")
+            if m.password == temp1: 
+                print("Login Successfully..!")
+                tempint = m.id
+                temp = m.name
+                input("Press any key to continue..!!")
+                return True , tempint, temp 
+            else:
+                print("Password is invalid..!!")
+                input("Press any key to continue..!!")
+                return False
+
+    if tempint == 0:
+        print("User Not found..!!")
+        input("Press any key to continue..!!")
+        return False
+
+def signUpCustomer(t, tlist):  # account creation 
+    temp = input("Enter name: ")
+    temp2 = input("Enter Password: ")
+    tempint = tlist.__len__()
+    # t.register(tempint, temp, temp2)
+    tlist.append(Customer(tempint, temp, temp2))
+    print("signup successfully ", end=" ")
+    tlist[tempint].show()
+    input("press key to continue..!!") 
+
+def signUpManager(t, tlist):  # account creation 
+    temp = input("Enter name: ")
+    temp2 = input("Enter Password: ")
+    tempint = tlist.__len__()
+    # t.register(tempint, temp, temp2)
+    tlist.append(Manager(tempint, temp, temp2))
+    print("signup successfully ", end=" ")
+    tlist[tempint].show()
+    input("press key to continue..!!") 
+
+def custmenu(custid, custname): # customer menu
+    while(True):
+        os.system("cls")
+        print(f"Hotel Management System (Customer) -- Id:{custid} Username:{custname}")
+        print("1. Rooms Detail")
+        print("2. Book Room")
+        print("3. Booking Status")
+        # print("4. Payment/Bill")
+        print("0. Back")
+
+        choice = int(input("Enter Choice: "))
+        match(choice):
+            case 0:
+                return
+            case 1 :
+                hms.RoomsDetail()
+                input("press any key to continue..!!")
+            case 2 :
+                tempint = int(input("Enter Room No: "))
+                hms.makeBooking(custid, tempint) # sending customer id and room number 
+                pass
+            case 3 :
+                hms.showApplicationStatus(custid)
+                input("Press any key to continue..!!")
+            
+        
+
+def managMenu(managid, managname): # Manager's menu
+    while(True):
+        os.system("cls")
+        print(f"Hotel Management System (Manager) -- Id:{managid} Username:{managname}")
+        print("1. Rooms Detail")
+        print("2. Show All Bookings ")
+        print("3. Show Booking Applications")
+        print("4. Manage Applications")
+        print("0. Back")
+
+        choice = int(input("Enter Choice: "))
+        match(choice):
+            case 0:
+                return
+            case 1 :
+                hms.RoomsDetail()
+                input("press any key to continue..!!")
+            case 2:
+                hms.showbookings()
+                input("press any key to continue..!!")
+            case 3 :
+                hms.displayAllApplication()
+                input("press any key to continue..!!")
+                pass
+            case 4 :
+                tempint, temp = hms.manageApplications() # taking customer id and room id 
+                if tempint != None and temp != None: # if room approved then this will run 
+                    hms.approveBooking(tempint, temp)
+                elif tempint == None :
+                    hms.clearRoom(temp) # giving room id to make it free 
+                input("press any key to continue..!!")
+                pass
+
+
+def login():
+    os.system("cls")
+    print("1. SignIn")
+    print("2. SignUp")
+    print("3. Show Users")
+    print("0. Back")
+    ch = int(input("Enter Choice: "))
+    return ch
+
+##### Main Function ########
+
+choice = 0 
+temp= ""
+temp1 = ""
+tempint = 0
+tempbool = None
+custlist = []
+managlist = []
+cust = Customer()
+manag = Manager()
+hms = HMS()
+
+while(True):
+    os.system("cls")
+
+    print("Hotel Management System")
+    print("1. Manager")
+    print("2. Customer")
+    print("0. Exit")
+    choice = int(input("Enter Choice: "))
+
+    if choice == 0:
+        exit()
+    elif choice == 1:  ## manager login
+        choice = login()
+        match(choice):
+            case 0 : ## back
+                exit()
+            case 1 : ## signIN old account
+                tempbool, tempint, temp = signIn(managlist)
+
+                if tempbool == True: # signin successfull manager 
+                    managMenu(tempint, temp) # sending manager  id and username 
+
+
+            case 2: ## signUp  ** create account
+                signUpManager(manag, managlist)
+            case 3:
+                for m in managlist:
+                    m.show()
+                input("Press any key to continue..!!")
+
+    elif choice == 2:  ## customer login
+        choice = login()
+        match(choice):
+            case 0 : ## back
+                exit()
+            case 1 : ## signIN old account
+                tempbool, tempint, temp = signIn(custlist)
+                
+                if tempbool == True: # signin successfull customer 
+                    custmenu(tempint, temp) # sending customer id and username 
+                    
+            case 2: ## signUp ** create account
+                signUpCustomer(cust, custlist)
+            case 3:
+                for c in custlist:
+                    c.show()
+                input("Press any key to continue..!!")
+    else:
+        print("Invalid Input..!!")
+        input("Press any key to Continue..!!")
